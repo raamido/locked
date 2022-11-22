47ba5f4873c4025b9adcb4b8954ee7a238e047c1
Rami <ramiabrahem24@gmail.com>
2022-11-23 00:45:17 +0200

    add routing paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Form from "./Components/Form";
+import Home from "./Components/Home";
 
 export default function App() {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Form />} />
+        <Route path="/home" element={<Home />}></Route>
+      </Routes>
+    </BrowserRouter>
+  );
 }
