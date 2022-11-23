972c792d608e0aa65ad83a4a01da979865c4294d
Rami <ramiabrahem24@gmail.com>
2022-11-23 16:20:41 +0200

    add header icon in Form Page

--- src/Components/Form.jsx
+++ src/Components/Form.jsx
@@ -6,6 +6,9 @@ export default function Form() {
     <div className="form">
       <section className="left"></section>
       <section className="right">
+        <header align="center">
+          <img src="header_icon.svg" alt="logo" width="120" />
+        </header>
         <Outlet />
       </section>
     </div>
