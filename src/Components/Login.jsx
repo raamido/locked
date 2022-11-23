13e16b9cbb4f37189138970ce71ab5091b9a78e3
Rami <ramiabrahem24@gmail.com>
2022-11-23 19:37:19 +0200

    add login form

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,5 +1,43 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 export default function Login() {
-  return <div>Login</div>;
+  const handleFormSubmission = (e) => {
+    e.preventDefault();
+    console.log("ready to submit!");
+  };
+
+  return (
+    <form onSubmit={handleFormSubmission}>
+      <h1 className="heading">LOGIN</h1>
+      <div className="text-field">
+        <input
+          type="email"
+          name="Email"
+          placeholder="Email"
+          required
+          pattern="^[a-z]{4,20}([0-9]{1,6})?@(gmail|yahoo|outlook).com$"
+        />
+      </div>
+      <div className="text-field">
+        <input
+          type="password"
+          name="Password"
+          placeholder="Password"
+          required
+          minLength="7"
+        />
+      </div>
+      <div align="right">
+        <button type="submit" className="cta-button">
+          UNLOCK
+        </button>
+      </div>
+      <p style={{ margin: "20px 0" }}>
+        <Link to="signup">
+          No Account? <b>Sign UP Now!</b>
+        </Link>
+      </p>
+    </form>
+  );
 }
