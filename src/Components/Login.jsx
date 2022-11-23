cee08dab2df1fcea694f9af92f43601d38b7689e
Rami <ramiabrahem24@gmail.com>
2022-11-23 19:56:41 +0200

    display current location in the title bar

--- src/Components/Login.jsx
+++ src/Components/Login.jsx
@@ -1,7 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 export default function Login() {
+  useEffect(() => {
+    document.title = "Locked — Login";
+  }, []);
+
   const handleFormSubmission = (e) => {
     e.preventDefault();
     console.log("ready to submit!");
