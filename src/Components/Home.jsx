9747ce30d3952d4929ad859b36b9e850a962751f
Rami <ramiabrahem24@gmail.com>
2022-11-27 18:14:15 +0200

    add home component template

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,5 +1,12 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 export default function Home() {
-  return <div>Home</div>;
+  return (
+    <div className="home">
+      <div className="card">
+        <h1>Hello Guest</h1>
+        <button className="cta-button">Logout</button>
+      </div>
+    </div>
+  );
 }
