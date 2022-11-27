72729e11d282a5b3e23d9282f754711e2056e99b
Rami <ramiabrahem24@gmail.com>
2022-11-27 21:40:02 +0200

    add logout functionality

--- src/Components/Home.jsx
+++ src/Components/Home.jsx
@@ -1,11 +1,32 @@
+import { signOut } from "firebase/auth";
 import React, { useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../Contexts/AuthContext";
+import { auth } from "../firebaseConfig";
 
 export default function Home() {
+  const navigate = useNavigate();
+  const [user, setUser] = useAuth();
+
+  const handleLogout = () => {
+    signOut(auth).then(() => {
+      setUser({
+        firstName: "",
+        lastName: "",
+        email: "",
+        loggedIn: false,
+      });
+      navigate("/");
+    });
+  };
+
   return (
     <div className="home">
       <div className="card">
-        <h1>Hello Guest</h1>
-        <button className="cta-button">Logout</button>
+        <h1>Hello {user.firstName}</h1>
+        <button className="cta-button" onClick={handleLogout}>
+          Logout
+        </button>
       </div>
     </div>
   );
